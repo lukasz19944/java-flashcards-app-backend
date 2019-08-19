301ad82b3676243d105bc17987492584c672832c
lukasz19944 <lukasz19944@gmail.com>
2019-08-19 21:34:59 +0200

    Szkielet aplikacji oraz konfiguracja połączenia z bazą danych

---
+++ src/main/java/pl/slusarski/javaflashcardsappbackend/JavaFlashcardsAppBackendApplication.java
@@ -0,0 +1,13 @@
+package pl.slusarski.javaflashcardsappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaFlashcardsAppBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaFlashcardsAppBackendApplication.class, args);
+    }
+
+}
